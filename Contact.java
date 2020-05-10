a594c3dee3290ac7c327663397c5377e52367789
shah-utkarsh <shahutkarshm@gmail.com>
2020-05-10 11:54:18 +0530

    First commit

---
+++ Contact.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.utkarsh.arraylistchallengecontact;
+
+/**
+ *
+ * @author Utkarsh
+ */
+public class Contact {
+    private  String name;
+    private  String phoneNumber;
+
+    public Contact(String name, String phoneNumber) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+    
+    public static Contact createContact(String name,String phoneNumber){
+        return new Contact( name, phoneNumber);
+    }
+}
